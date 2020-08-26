c5b1ae3e4330ebba5b812739ef229d68c0c86f80
Samuel Andert <samuelandert@me.com>
2020-08-26 16:19:37 +0200

    save bg on omo sapien

--- app/src/components/OmoSapiens.svelte
+++ app/src/components/OmoSapiens.svelte
@@ -1,15 +1,17 @@
-<div class="bg-white w-full p-8">
-  <div class="font-title text-tertiary text-xl">Trusted Omos</div>
-  <div class="flex h-14 mb-1 w-full text-secondary">
-    <img alt="" src="https://i.pravatar.cc/150?" class="h-12 w-auto" />
-    <div class="text-lg py-3 px-4 flex-1">
-      <p>Omo Sapien 1</p>
+<div class="p-12 bg-gray-200">
+  <div class="bg-white w-full p-8">
+    <div class="font-title text-tertiary text-xl">Trusted Omos</div>
+    <div class="flex h-14 mb-1 w-full text-secondary">
+      <img alt="" src="https://i.pravatar.cc/150?" class="h-12 w-auto" />
+      <div class="text-lg py-3 px-4 flex-1">
+        <p>Omo Sapien 1</p>
+      </div>
     </div>
-  </div>
-  <div class="flex h-12 mb-4 w-full bg-gray-100 text-secondary">
-    <img alt="" src="https://i.pravatar.cc/150?" class="h-full w-auto" />
-    <div class="text-lg py-3 px-4 flex-1">
-      <p>Omo Sapien 2</p>
+    <div class="flex h-12 mb-4 w-full bg-gray-100 text-secondary">
+      <img alt="" src="https://i.pravatar.cc/150?" class="h-full w-auto" />
+      <div class="text-lg py-3 px-4 flex-1">
+        <p>Omo Sapien 2</p>
+      </div>
     </div>
   </div>
 </div>
