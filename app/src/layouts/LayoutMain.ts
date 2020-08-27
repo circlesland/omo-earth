4d0abaf44916cded2d57f90717b495944147f76c
Samuel Andert <samuelandert@me.com>
2020-08-27 16:28:59 +0200

    fixed responsive layout

--- app/src/layouts/LayoutMain.ts
+++ app/src/layouts/LayoutMain.ts
@@ -2,5 +2,5 @@ export const LayoutMain = {
   areas: "'main'",
   columns: "1fr",
   rows: "1fr",
-  class: "p-12",
+  // class: "p-12",
 };
--- app/src/pages/Product.ts
+++ app/src/pages/Product.ts
@@ -1,6 +1,6 @@
 import { generatePageSingle } from "../templates/TemplatePageDetail";
-import { menu2 } from "../organisms/menu2";
+import { menu } from "../organisms/menu";
 import { quickActionsSafe } from "../organisms/quickActionsSafe";
 
 export const Product = (content: any) =>
-  generatePageSingle("SafeContext", menu2, content, quickActionsSafe);
+  generatePageSingle("SafeContext", menu, content, quickActionsSafe);
